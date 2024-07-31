e1b207ccab61e24f63434f5b788c7921f1968c70
LadaDzenis <lada.tranevskaya@mail.ru>
2024-07-31 21:14:06 +0300

    Add files via upload

---
+++ module_1_3.py
@@ -0,0 +1,8 @@
+name = "Lada"
+print ("Name: " + name)
+age = int(24)
+print ("Age: " + str(age))
+age = age + 1
+print ("New age: " + str(age))
+is_student = name==name and age==age
+print ("Is_student: " + str(is_student))
